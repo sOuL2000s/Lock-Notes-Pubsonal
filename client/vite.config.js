2bb6e4bb456759b285e741a41484d5b0b51c74b7
Souparna Paul <souparnopaulreborn@gmail.com>
2026-09-06 00:32:12 +0530

    Fix: Output dist to root folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist',
+    outDir: '../dist',  // Output to root dist
     sourcemap: false,
     rollupOptions: {
       output: {
