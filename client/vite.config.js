7fd9eb9239f3e78d824f0b9b154d176ef575e1f2
Souparna Paul <souparnopaulreborn@gmail.com>
2026-09-06 00:38:27 +0530

    Fix: Use root dist directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist',  // Output to client/dist
+    outDir: '../dist',  // Output to root dist
     sourcemap: false,
     rollupOptions: {
       output: {
