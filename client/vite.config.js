cf8579390f8f18300735933713517da2cc2423ef
Souparna Paul <souparnopaulreborn@gmail.com>
2026-09-06 00:02:58 +0530

    vercel fixes done

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,5 +10,17 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'axios']
+        }
+      }
+    }
   }
 });
